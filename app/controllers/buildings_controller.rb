9af1a0b9eafe45779a2b78997221caf372e63134
seanraad123 <seanconrad123@gmail.com>
2018-07-26 10:38:17 -0400

    updated buildings controller and andded edit page and form

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,30 @@
 class BuildingsController < ApplicationController
+  before_action :set_building, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @buildings = Building.all
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+    if @building.update(building_params)
+      redirect_to buildings_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def building_params
+    params.require(:building).permit(:name, :country, :address, :rent_per_floor, :number_of_floors)
+  end
+
+  def set_building
+    @building = Building.find(params[:id])
+  end
+
 end
